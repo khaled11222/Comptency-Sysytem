138dd38718e2291b871507c18dc178a5affe7e17
OmarSaber1 <osaber218@gmail.com>
2024-09-21 14:36:06 +0300

    add tabs

--- src/CustomTabs.js
+++ src/CustomTabs.js
@@ -1,75 +1,121 @@
-import React from 'react';
-import { Tabs, Tab, Box } from '@mui/material';
-import BookIcon from '@mui/icons-material/Book';
-import BriefcaseIcon from '@mui/icons-material/Work';
-import PeopleIcon from '@mui/icons-material/People';
+import React from "react";
+import { Tabs, Tab, Box, Typography } from "@mui/material";
+import BookIcon from "@mui/icons-material/Book";
+import BriefcaseIcon from "@mui/icons-material/Work";
+import PeopleIcon from "@mui/icons-material/People";
+import { CardList } from "./components/CardList/CardList";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value != index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value == index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
 
 function CustomTabs() {
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = React.useState(1);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   return (
-    <Box sx={{ width: '100%', padding: '16px' }}>
+    <Box sx={{ width: "100%", padding: "16px" }}>
       <Tabs
         value={value}
         onChange={handleChange}
         aria-label="custom tabs example"
         centered
-        TabIndicatorProps={{ style: { display: 'none' } }} // Hide the underline
-        sx={{ '& .MuiTab-root': { margin: '0 8px', minWidth: '100px', borderRadius: '8px' } }} // Decrease size and add border radius
+        TabIndicatorProps={{ style: { display: "none" } }} // Hide the underline
+        sx={{
+          "& .MuiTab-root": {
+            margin: "0 8px",
+            minWidth: "100px",
+            borderRadius: "8px",
+            minHeight: "40px",
+            height: "40px",
+          },
+        }} // Decrease size and add border radius
       >
         <Tab
           icon={<BookIcon />}
           iconPosition="start"
           label="Academic Courses"
+          value="1"
           sx={{
-            color: 'darkblue',
-            fontSize: '14px', // Decrease font size
-            fontWeight: 'bold',
-            flexDirection: 'row',
-            padding: '6px 12px', // Decrease height
-            margin: '4px', // Separate tabs
-            '&:hover': { backgroundColor: '#e0f7fa' }, // Background color on hover
-            '&.Mui-selected': { backgroundColor: '#e0f7fa' }, // Background color when selected
-            borderRadius: '8px' // Border radius for each tab
+            color: "#4B5563",
+            textTransform: "none",
+            fontSize: "14px", // Decrease font size
+            fontWeight: 600,
+            flexDirection: "row",
+            padding: "8px 12px 8px 8px",
+            borderRadius: 6,
+            margin: "4px", // Separate tabs
+            backgroundColor: "white",
+            "&.Mui-selected": { backgroundColor: "#E0F2FE", color: "#00629C" },
           }}
         />
         <Tab
           icon={<BriefcaseIcon />}
           iconPosition="start"
           label="Extra Courses"
+          value="2"
           sx={{
-            color: 'darkblue',
-            fontSize: '14px', // Decrease font size
-            fontWeight: 'bold',
-            flexDirection: 'row',
-            padding: '6px 12px', // Decrease height
-            margin: '4px', // Separate tabs
-            '&:hover': { backgroundColor: '#e0f7fa' }, // Background color on hover
-            '&.Mui-selected': { backgroundColor: '#e0f7fa' }, // Background color when selected
-            borderRadius: '8px' // Border radius for each tab
+            color: "#4B5563",
+            textTransform: "none",
+            maxHeight: 40,
+            fontSize: "14px", // Decrease font size
+            fontWeight: 600,
+            flexDirection: "row",
+            padding: "8px, 12px, 8px, 8px",
+            borderRadius: 6,
+            margin: "4px", // Separate tabs
+            backgroundColor: "white",
+            "&.Mui-selected": { backgroundColor: "#E0F2FE", color: "#00629C" },
           }}
         />
         <Tab
           icon={<PeopleIcon />}
           iconPosition="start"
           label="Clubs"
+          value="3"
           sx={{
-            color: 'darkblue',
-            fontSize: '14px', // Decrease font size
-            fontWeight: 'bold',
-            flexDirection: 'row',
-            padding: '6px 12px', // Decrease height
-            margin: '4px', // Separate tabs
-            '&:hover': { backgroundColor: '#e0f7fa' }, // Background color on hover
-            '&.Mui-selected': { backgroundColor: '#e0f7fa' }, // Background color when selected
-            borderRadius: '8px' // Border radius for each tab
+            color: "#4B5563",
+            textTransform: "none",
+            maxHeight: 40,
+            fontSize: "14px", // Decrease font size
+            fontWeight: 600,
+            flexDirection: "row",
+            padding: "8px, 12px, 8px, 8px",
+            borderRadius: 6,
+            margin: "4px", // Separate tabs
+            backgroundColor: "white",
+            "&.Mui-selected": { backgroundColor: "#E0F2FE", color: "#00629C" },
           }}
         />
       </Tabs>
+      <TabPanel index={1} value={value}>
+        <CardList />
+      </TabPanel>
+      <TabPanel index={2} value={value}>
+        <CardList withImage />
+      </TabPanel>
+      <TabPanel index={3} value={value}>
+        Item Three
+      </TabPanel>
     </Box>
   );
 }
--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -3,22 +3,22 @@ import { CourseCard } from "../Card/Card";
 
 export const CardList = ({ withImage }) => (
   <Grid container spacing={2}>
-    <Grid item sm={6}>
+    <Grid item sm={withImage ? 12 : 6}>
       <CourseCard isImageVisible={withImage} />
     </Grid>
-    <Grid item sm={6}>
+    <Grid item sm={withImage ? 12 : 6}>
       <CourseCard isImageVisible={withImage} />
     </Grid>
-    <Grid item sm={6}>
+    <Grid item sm={withImage ? 12 : 6}>
       <CourseCard isImageVisible={withImage} />
     </Grid>
-    <Grid item sm={6}>
+    <Grid item sm={withImage ? 12 : 6}>
       <CourseCard isImageVisible={withImage} />
     </Grid>
-    <Grid item sm={6}>
+    <Grid item sm={withImage ? 12 : 6}>
       <CourseCard isImageVisible={withImage} />
     </Grid>
-    <Grid item sm={6}>
+    <Grid item sm={withImage ? 12 : 6}>
       <CourseCard isImageVisible={withImage} />
     </Grid>
   </Grid>
